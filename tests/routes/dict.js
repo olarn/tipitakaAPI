9bb2f1593c45b2696f687a7d139e25bcc5669b99
Olarn U <olarn@Olarns-Mac-mini.local>
2016-11-28 23:15:32 +0700

    add dict test

--- tests/routes/dict.js
+++ tests/routes/dict.js
@@ -1,10 +1,31 @@
-'use restrict'
+'use strict'
 
 var expect = require('chai').expect
+var http = require('http')
 var dict = require('../../routes/dict/dict.js')
 
 describe('dict API', function() {
+
+    before(function(done) {
+        var server = http.createServer(function(req, res){
+        res.end('Hello World\n');
+    })
+
+    server.listen(3000);
+        done()
+    });
+
+    after(function(done) {
+        done()
+    });
+
     it('get() thai2thai with "ทดสอบ" should return meaning of it', function() {
-        
+        http.get("http://localhost:3000/", function(res) {
+            res.on('data', function(body) {
+                console.log(body)
+                expect(body).toEqual("test")
+                done()
+            })
+        })
     })
 })
