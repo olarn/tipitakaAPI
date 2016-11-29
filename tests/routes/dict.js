5f3137bbd5db0e588a50183c4469da65c6ba8091
Olarn U <olarn.u@gmail.com>
2016-11-29 13:32:25 +0700

    replace http in dict test with request.

--- tests/routes/dict.js
+++ tests/routes/dict.js
@@ -1,31 +1,27 @@
 'use strict'
 
 var expect = require('chai').expect
-var http = require('http')
+var request = require('request')
 var dict = require('../../routes/dict/dict.js')
 
 describe('dict API', function() {
 
-    before(function(done) {
-        var server = http.createServer(function(req, res){
-        res.end('Hello World\n');
+    before(() => {
+        
     })
 
-    server.listen(3000);
-        done()
-    });
+    after(() => {
 
-    after(function(done) {
-        done()
-    });
+    })
 
-    it('get() thai2thai with "ทดสอบ" should return meaning of it', function() {
-        http.get("http://localhost:3000/", function(res) {
-            res.on('data', function(body) {
+    it('get() thai2thai with "ทดสอบ" should return meaning of it', function(done) {
+        request('http://httpbin.org/ip', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
                 console.log(body)
-                expect(body).toEqual("test")
-                done()
-            })
+            } else {
+                console.log(error)
+            }
+            done()
         })
     })
 })
