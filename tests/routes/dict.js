9be2295a9e93c6afdee1ac84acc1b04f24466332
Olarn U <olarn.u@gmail.com>
2016-11-29 17:30:51 +0700

    finish integration test. ready to refactor dict.

--- tests/routes/dict.js
+++ tests/routes/dict.js
@@ -1,27 +1,23 @@
 'use strict'
 
-var expect = require('chai').expect
-var request = require('request')
-var dict = require('../../routes/dict/dict.js')
+// https://www.npmjs.com/package/supertest
+
+const expect = require('chai').expect
+const supertest = require('supertest');
+const app = require('../../app')
 
 describe('dict API', function() {
-
-    before(() => {
-        
-    })
-
-    after(() => {
-
-    })
-
     it('get() thai2thai with "ทดสอบ" should return meaning of it', function(done) {
-        request('http://httpbin.org/ip', function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body)
-            } else {
-                console.log(error)
-            }
-            done()
-        })
+        const expectedHeader = 'ทดสอบ'
+        const expectedTranslate = 'ก. ตรวจสอบเพื่อความถูกต้องแน่นอน เช่น ทดสอบเครื่องยนต์; (การศึกษา) สอบเพื่อวัดความรู้ความสามารถเป็นต้น เช่น ทดสอบความรู้ของนักเรียนด้วยการให้ลองปฏิบัติ.'
+        const path = '/dict/thai2thai/' + encodeURIComponent("ทดสอบ")
+        supertest(app)
+            .get(path)
+            .expect(200, 
+                [{
+                    head: expectedHeader,
+                    translation: expectedTranslate 
+                }],
+                done)
     })
 })
